56c2741384d07954b54bbf206db9ef0fcc8101df
zkobrinsky <zachkobrinsky@gmail.com>
2021-04-15 12:40:14 -0400

    added opn to server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-var express = require("express");
-var app = express();
-var path = require("path");
+const express = require("express");
+const app = express();
+const path = require("path");
+const opn = require("opn");
+const url = "http://localhost:8000/";
+
 app.use("/static", express.static("./static/"));
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:8000
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(8000);
+
+opn(url);
